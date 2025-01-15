429b1d16505c3b8e7f596db63d48e2f94e3fd4e0
gityeop <freiheit517@icoud.com>
2025-01-15 23:58:57 +0900

    solve 2839

--- 2839.py
+++ 2839.py
@@ -1,5 +1,6 @@
 n = int(input())
 dp = [float("inf")] * (n + 1)
+# dp[i]: i kg을 만들 수 있는 최소 봉지 개수
 dp[0] = 0
 
 for i in range(3, n + 1):
@@ -8,9 +9,9 @@ for i in range(3, n + 1):
 
 for i in range(5, n + 1):
     if dp[i - 5] != float("inf"):
-        dp[i] = min(dp[i], dp[i - 5] + 1)
+        dp[i] = min(dp[i], dp[i - 5] + 1)  # 5kg 봉지 사용 시의 최소값 계산
 
-if dp[n] == float("inf"):
+if dp[n] == float("inf"):  # 만들 수 없는 경우
     print(-1)
 else:
-    print(dp[n])
+    print(dp[n])  # 최소 봉지 개수 출력
